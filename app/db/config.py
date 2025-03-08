c1a7238b3a5354f8008491e0cfbda4b3c66a5244
thanhlam110704 <ltthanh1107@gmail.com>
2025-03-08 10:41:24 +0700

    code

--- app/db/config.py
+++ app/db/config.py
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
     app_database_name: Optional[str] = None
     database_url: Optional[str] = None
     log_database_name: Optional[str] = None
+    
 
 
 settings = Settings()
