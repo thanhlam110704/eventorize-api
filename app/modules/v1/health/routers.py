259d4493233f9cc0483d7ae537aa3c19c738faac
thanhlam110704 <ltthanh1107@gmail.com>
2025-05-06 20:09:55 +0700

    fix code

--- app/modules/v1/health/routers.py
+++ app/modules/v1/health/routers.py
@@ -1,6 +1,6 @@
 from fastapi_restful.cbv import cbv
 from fastapi_restful.inferring_router import InferringRouter
-
+from fastapi import Response
 router = InferringRouter(
     prefix="/v1/health",
     tags=["v1/health"],
@@ -12,3 +12,7 @@ class RoutersCBV:
     @router.get("/ping")
     async def health_check(self):
         return {"ping": "pong!"}
+    
+    @router.head("/ping")
+    async def health_check_head(self):
+        return Response(status_code=200)
--- app/partners/v1/resend/config.py
+++ app/partners/v1/resend/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     resend_api_key: Optional[str] = None
     host_email: str = Field(default="no-reply@eventorize-api-thanhlam.site")
     sender_name: str = Field(default="Eventorize")
-    verification_link: str = Field(default="https://eventorize.giapkun.site/verify-email")
-    reset_password_link: str = Field(default="https://eventorize.giapkun.site/reset-password")
+    verification_link: str = Field(default="https://eventorize-api-thanhlam.siteverify-email")
+    reset_password_link: str = Field(default="https://eventorize-api-thanhlam.site/reset-password")
 
 settings = Settings()
