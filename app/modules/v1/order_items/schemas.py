1983bd1bd4a8785a5c4584824a83577d6cb26bd6
thanhlam110704 <ltthanh1107@gmail.com>
2025-07-07 23:35:47 +0700

    fix api

--- app/modules/v1/order_items/schemas.py
+++ app/modules/v1/order_items/schemas.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from core.schemas import ObjectIdStr
 from pydantic import BaseModel, Field
+from core.schemas import UrlStr
 
 
 class CreateRequest(BaseModel):
@@ -17,6 +18,10 @@ class Response(BaseModel):
     ticket_title: str
     event_id: str
     event_title: str
+    event_thumbnail: UrlStr
+    event_address: str 
+    event_start_date: datetime
+    event_end_date: datetime
     status: str
     quantity: int
     price: float
